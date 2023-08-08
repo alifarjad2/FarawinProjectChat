b7e6b4911542b3d7c955e46620580db33d1bcac6
alireza <test@email.com>
2023-08-08 18:27:34 +0330

    2

--- alirezaSepehri2/src/FormAuth.jsx
+++ alirezaSepehri2/src/FormAuth.jsx
@@ -23,11 +23,13 @@ export default function FormAuth() {
 function BoxLogin() {
   return <div className="w-72 h-4/5 bg-white rounded-md pt-11 px-6">
     <header>
-      <h1 className="text-center font-bold pb-3">Login</h1>
+      <h1 className="text-center font-bold pb-2 text-lg text-stone">Login</h1>
     </header>
 
-    <InputBox label={'username'} type={'text'} placeholder={'Type your username'}/>
-    <InputBox label={'password'} type={'password'}  placeholder={'Type your password'}/>
+    <InputBox label={'username'} type={'text'} placeholder={'Type your username'} 
+       reaction={getUsername} />
+    <InputBox label={'password'} type={'password'}  placeholder={'Type your password'}
+       reaction={getPassword}/>
     
     <div className="text-right">
         <ForgotPass />
@@ -36,7 +38,7 @@ function BoxLogin() {
     <LoginBtn />
 
     <button
-        className="mx-2 w-full text-center text-xs"
+        className="mx-2 w-full text-center text-xs text-slate"
         onClick={() => {
           farawin.testRegister("09393013397", "12345678", "Ali Farjad");
         }}
@@ -48,23 +50,37 @@ function BoxLogin() {
 }
 
 // کامپوننت برای تولید کادرهای دریافت نام کاربری و پسورد
-function InputBox({label, type, placeholder}) {
+function InputBox({label, type, placeholder, reaction}) {
   return (
-    <div className="mt-2.5">
-      <label className="text-xs ml-2" 
+    <div className="mt-2">
+      <label className="text-xs ml-2 text-slate" 
         htmlFor={label}>{label}</label>
-      <input className="w-full p-2 text-sm outline-none"
+      <input className="w-full p-2 text-sm outline-none text-zinc border-b"
         id={label}
         type={type} 
         placeholder={placeholder}
+        onInput={reaction}
       />
     </div>
   )
 }
 
+// بررسی وضعیت نام کاربری ورودی
+function getUsername() {
+  const telFormat = /^09([0-9]{9})$/
+
+  console.log('check')
+}
+
+// بررسی وضعیت رمز ورود
+function getPassword() {
+
+  console.log('check') 
+}
+
 // کامپوننت برای تولید دکمه فراموشی رمز 
 function ForgotPass() {
-  return <span className="text-xs cursor-pointer" 
+  return <span className="text-xs cursor-pointer text-slate" 
             onClick={() => {alert('این امکان هنوز پیاده سازی نشده است')}}>
           Forgot password?
         </span>
--- alirezaSepehri2/tailwind.config.js
+++ alirezaSepehri2/tailwind.config.js
@@ -6,6 +6,21 @@ export default {
   ],
   theme: {
     extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'purple': '#3f3cbb',
+      'midnight': '#121063',
+      'metal': '#565584',
+      'tahiti': '#3ab7bf',
+      'silver': '#ecebff',
+      'bubble-gum': '#ff77e9',
+      'bermuda': '#78dcca',
+      'slate': '#64748b',
+      'zinc': '#3f3f46',
+      'stone': '#292524'
+    },
   },
   plugins: [],
 }
