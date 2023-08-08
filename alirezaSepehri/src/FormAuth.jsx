b84bb577a8f3ffdc3ffffdb4b1e7ab28ecb1b00f
alireza <test@email.com>
2023-08-08 09:59:38 +0330

    alireza-sepehri- repeat2

--- alirezaSepehri/src/FormAuth.jsx
+++ alirezaSepehri/src/FormAuth.jsx
@@ -1,11 +1,11 @@
-import farawin from "farawin";
+import farawin from "./farawin";
 
 // 1- کاربر بتونه درست لاگین و رجیستر کنه
 // ۲- کارکرد صحیح سناریو های گفته شده
 export default function FormAuth() {
   return (
     <div
-      className="h-full"
+      className="h-full flex justify-center items-center"
       style={{
         background: `url(
           https://colorlib.com/etc/lf/Login_v4/images/bg-01.jpg
@@ -13,8 +13,9 @@ export default function FormAuth() {
       }}
     >
       {/* TODO Insert Form Here*/}
+      <BoxLogin />
 
-      <button
+      {/* <button
         className="mx-2 w-10"
         onClick={() => {
           farawin.testLogin("09393013397", "12345678");
@@ -30,7 +31,15 @@ export default function FormAuth() {
         }}
       >
         test register
-      </button>
+      </button> */}
     </div>
   );
+}
+
+function BoxLogin() {
+  return <div className="w-72 h-4/5 bg-white rounded-md p-4">
+    <header>
+      <h1 className="text-center font-bold">LOGIN</h1>
+    </header>
+  </div>
 }
