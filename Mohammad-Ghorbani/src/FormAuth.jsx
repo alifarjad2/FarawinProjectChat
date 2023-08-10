fb882aae737f9e7ec660636f1f478512ac42ad2d
Mohamad Ghorbani <mohamad.ghorbani.official@gmail.com>
2023-08-10 17:09:15 +0330

    create UI

--- Mohammad-Ghorbani/src/FormAuth.jsx
+++ Mohammad-Ghorbani/src/FormAuth.jsx
@@ -1,36 +1,16 @@
-import farawin from "farawin";
+// import farawin from "farawin";
+import { useState } from "react";
+import Login from "./Login";
+import Register from "./Register";
 
-// 1- کاربر بتونه درست لاگین و رجیستر کنه
-// ۲- کارکرد صحیح سناریو های گفته شده
 export default function FormAuth() {
-  return (
-    <div
-      className="h-full"
-      style={{
-        background: `url(
-          https://colorlib.com/etc/lf/Login_v4/images/bg-01.jpg
-        )`,
-      }}
-    >
-      {/* TODO Insert Form Here*/}
-
-      <button
-        className="mx-2 w-10"
-        onClick={() => {
-          farawin.testLogin("09393013397", "12345678");
-        }}
-      >
-        test login
-      </button>
-
-      <button
-        className="mx-2 w-10"
-        onClick={() => {
-          farawin.testRegister("09393013397", "12345678", "Ali Farjad");
-        }}
-      >
-        test register
-      </button>
-    </div>
+  function switchForm(val) {
+    setProducts(val);
+  }
+  const [products, setProducts] = useState("Login");
+  return products === "Login" ? (
+    <Login props={switchForm} />
+  ) : (
+    <Register props={switchForm} />
   );
 }
