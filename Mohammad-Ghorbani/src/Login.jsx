a0bddf09467143aced3d0812a71a151168f44465
Mohamad Ghorbani <mohamad.ghorbani.official@gmail.com>
2023-08-10 23:42:48 +0330

    Initial version created

--- Mohammad-Ghorbani/src/Login.jsx
+++ Mohammad-Ghorbani/src/Login.jsx
@@ -1,23 +1,33 @@
+import farawin from "farawin";
 import { useState } from "react";
 
 function Login({ props }) {
   const [username, setUsername] = useState("");
-  // const [password, setPassword] = useState("");
+  const [password, setPassword] = useState("");
+  //farawin.toEnDigit(username);
+  const usernameHandler = (val) => {
+    setUsername(val);
+    //console.log(username);
+  };
 
-  const handler = (value) => {
-    setUsername(value);
+  const passwordHandler = (val) => {
+    setPassword(val);
+  };
+
+  const validateLogin = async () => {
+    const usernameToEn = farawin.toEnDigit(username);
+    const usernameValidation = farawin.mobileRegex;
+
+    if (usernameValidation.test(usernameToEn)) {
+      await farawin.testLogin(usernameToEn, password, (res) =>
+        console.log(res)
+      );
+    }
   };
 
   return (
-    <div
-      className="h-full flex "
-      style={{
-        background: `url(
-            https://colorlib.com/etc/lf/Login_v4/images/bg-01.jpg
-          )`,
-      }}
-    >
-      <form className=" w-3/12 h-5/6 bg-white m-auto rounded-lg  pt-10 flex flex-col p-8 gap-14 ">
+    <div className="h-full flex">
+      <form className=" w-3/12 h-5/6 bg-white m-auto rounded-lg  pt-10 flex flex-col p-8 gap-14  overflow-hidden">
         <h1 className="text-center text-5xl font-bold ">Login</h1>
         <div>
           <label className="text-sm" htmlFor="username">
@@ -27,7 +37,7 @@ function Login({ props }) {
             id="username"
             type="text"
             onChange={(e) => {
-              handler(e.target.value);
+              usernameHandler(e.target.value);
             }}
             value={username}
             placeholder="Type your Username"
@@ -35,18 +45,31 @@ function Login({ props }) {
           />
         </div>
         <div>
-          <label className="text-sm" htmlFor="password">
+          <label className="text-sm " htmlFor="password">
             Password
           </label>
           <input
-            type="text"
+            onChange={(e) => {
+              passwordHandler(e.target.value);
+            }}
+            value={password}
+            type="password"
             placeholder="Type your password"
             className="w-full  focus:outline-none border-b-2"
             id="password"
           />
-          <div className="text-end">Forget password?</div>
+          <button
+            type="button"
+            onClick={() => {
+              alert("این امکان هنوز پیاده سازی نشده است");
+            }}
+            className="w-fit h-fit mt-4 max-lg:m-auto max-lg:mt-3"
+          >
+            Forget password?
+          </button>
         </div>
         <button
+          onClick={validateLogin}
           type="button"
           style={{
             background: `-webkit-linear-gradient(right,#00dbde,#fc00ff,#00dbde,#fc00ff)`,
@@ -58,7 +81,7 @@ function Login({ props }) {
         <div className="text-center">
           <p className="text-gray-500">Or Sing Up Using</p>
           <button
-            type="submit"
+            type="button"
             onClick={() => {
               props("Register");
             }}
--- Mohammad-Ghorbani/src/Register.jsx
+++ Mohammad-Ghorbani/src/Register.jsx
@@ -1,21 +1,50 @@
+import { useState } from "react";
+
 function Register({ props }) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [repeatPassword, setRepeatPassword] = useState("");
+
+  const usernameHandler = (val) => {
+    setUsername(val);
+  };
+  const passwordHandler = (val) => {
+    setPassword(val);
+  };
+  const repeatPasswordHandler = (val) => {
+    setRepeatPassword(val);
+  };
+
+  const validation = async () => {
+    const usernameToEn = farawin.toEnDigit(username);
+    const usernameValidation = farawin.mobileRegex;
+    if (repeatPassword !== password) {
+      alert("The password and password repeat don't match");
+    } else if (usernameValidation.test(usernameToEn)) {
+      await farawin.testRegister(usernameToEn, password, "User", (res) => {
+        alert(res.message);
+        if (res.code == 200) {
+          setUsername("");
+          setPassword("");
+          setRepeatPassword("");
+        }
+      });
+    }
+  };
+
   return (
-    <div
-      className="h-full flex "
-      style={{
-        background: `url(
-            https://colorlib.com/etc/lf/Login_v4/images/bg-01.jpg
-          )`,
-      }}
-    >
-      <form
-        action=""
-        className=" w-3/12 h-5/6 bg-white m-auto rounded-lg  pt-10 flex flex-col p-8 gap-8 "
-      >
+    <div className="h-full flex ">
+      <form className=" w-3/12 h-5/6 bg-white m-auto rounded-lg  pt-10 flex flex-col p-8 gap-14 ">
         <h1 className="text-center text-5xl font-bold ">Register</h1>
         <div>
-          <div className="text-sm">Username</div>
+          <label className="text-sm" htmlFor="username">
+            Username
+          </label>
           <input
+            onChange={(e) => {
+              usernameHandler(e.target.value);
+            }}
+            value={username}
             id="username"
             type="text"
             placeholder="Type your Username"
@@ -23,36 +52,43 @@ function Register({ props }) {
           />
         </div>
         <div>
-          <div className="text-sm">Password</div>
+          <label className="text-sm" htmlFor="password">
+            Password
+          </label>
           <input
+            onChange={(e) => {
+              passwordHandler(e.target.value);
+            }}
+            value={password}
+            id="password"
             type="password"
             placeholder="Type your password"
             className="w-full  focus:outline-none border-b-2"
           />
         </div>
         <div>
-          <div className="text-sm">Repeat Password</div>
+          <label className="text-sm" htmlFor="repeatPassword">
+            Repeat Password
+          </label>
           <input
+            onChange={(e) => {
+              repeatPasswordHandler(e.target.value);
+            }}
+            value={repeatPassword}
+            id="repeatPassword"
             type="password"
-            placeholder="Type your password"
-            className="w-full  focus:outline-none border-b-2"
-          />
-        </div>
-        <div>
-          <div className="text-sm">Name</div>
-          <input
-            type="text"
-            placeholder="Type your name"
+            placeholder="Repeat your password"
             className="w-full  focus:outline-none border-b-2"
           />
         </div>
 
         <button
+          onClick={validation}
           type="button"
           style={{
             background: `-webkit-linear-gradient(right,#00dbde,#fc00ff,#00dbde,#fc00ff)`,
           }}
-          className="w-full h-16  rounded-3xl text-white"
+          className="w-full h-11  rounded-3xl text-white"
         >
           SIGN UP
         </button>
