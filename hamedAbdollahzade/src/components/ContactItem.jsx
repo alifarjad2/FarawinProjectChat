2b86109bfcfb5107edd1eaa04f337dd73fcbbed0
Hame:D <hamedsmart20@gmail.com>
2023-08-20 16:40:52 +0330

    نمایش لیست مخاطبین و دو حرف اول در اواتار

--- hamedAbdollahzade/src/components/ContactItem.jsx
+++ hamedAbdollahzade/src/components/ContactItem.jsx
@@ -1,24 +1,16 @@
-export const ContactItem = () => {
-    return (
-        <div className="cursor-pointer">
-
-        <div
-          className="hover:bg-[rgba(28,62,156,0.2)] flex flex-row h-12 w-full overflow-hidden rounded-lg mt-1 relative"
-        >
-         <div className="bg-red-400 w-10 h-10 mx-1 pt-1 rounded-full ">
-            م م
-         </div>
-          <div className="flex flex-col ">
-            <p>مخاطب مورد نظر</p>
-            <p className="text-xs text-start">متن اخرین پیام ... </p>
-          </div>
-          <div
-            className="rounded-full w-4 h-4 text-xs absolute left-2 bottom-2 bg-blue-500"
-          >
-            9
-          </div>
+export const ContactItem = ({ key, name }) => {
+  return (
+    <div key={key} className="cursor-pointer">
+      <div className="hover:bg-[rgba(28,62,156,0.2)] flex flex-row h-12 w-full overflow-hidden rounded-lg mt-1 relative">
+        <div className={Math.random() * 1 > 0.5  ? "bg-red-400 flex flex-col justify-center items-center w-10 h-10 text-sm mx-1 pt-1 rounded-full " : "bg-amber-400 flex flex-col justify-center items-center w-10 h-10 text-sm mx-1 pt-1 rounded-full " }>{name[0]}{name[1]}</div>
+        <div className="flex flex-col ">
+          <p>{name}</p>
+          <p className="text-xs text-start">متن اخرین پیام ... </p>
         </div>
-    
+        <div className="rounded-full w-4 h-4 text-xs absolute left-2 bottom-2 bg-blue-500">
+          9
+        </div>
       </div>
-    )
-}
+    </div>
+  );
+};
--- hamedAbdollahzade/src/components/drawer.jsx
+++ hamedAbdollahzade/src/components/drawer.jsx
@@ -1,6 +1,27 @@
+import { useState, useEffect } from "react";
 import { ContactItem } from "./ContactItem";
+import farawin from "farawin";
 
 export const DrawerChat = () => {
+  const [members, setMembers] = useState([]);
+  
+  
+
+  const getMembers = async () => {
+    const res = await farawin.getContacts();
+    const filterContact = res.contactList.filter((r) => {
+      return r.ref == localStorage.userMobile;
+    });
+
+    // console.table(res.contactList);
+    // console.log(filterContact);
+    setMembers(filterContact);
+  };
+
+  useEffect(() => {
+    getMembers();
+  }, []);
+
   return (
     <div className="overflow-auto  m-1 rounded-lg w-1/3 text-center p-2 max-sm:hidden">
       {/* این دیو مخصوص برای قسمت سرچ میباشد  */}
@@ -20,7 +41,11 @@ export const DrawerChat = () => {
         />
       </div>
 
-      <ContactItem />
+      {members.length == 0
+        ? "هنوز مخاطبی وجود ندارد"
+        : members.map((contact) => (
+            <ContactItem key={contact.index} name={contact.name} />
+          ))}
     </div>
   );
 };
