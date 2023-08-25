7f361864ef574e0a4f82c7d9c1fc9513c9563388
samane ghazanfari <samanegha12@gmail.com>
2023-08-25 23:38:54 +0430

    نمایش نتایج سرچ مخاطب

--- samaneGhazanfar/src/Components/SearchBar.jsx
+++ samaneGhazanfar/src/Components/SearchBar.jsx
@@ -2,29 +2,48 @@ import { useState } from "react";
 
 export const SearchBar = ({propContact})=>{
     const [searchInput,setSearchInput] = useState("");
+    const [searchResults, setSearchResults] = useState([]);
+
+    const handellSearch =()=>{
+        let resultSearch = searchInput.length === 0 ? {propContact} : propContact.filter((contact)=>
+    (contact.name.toLowerCase().includes(searchInput.toLowerCase())));
+      console.log(resultSearch);
+      setSearchResults(resultSearch)
+    }
 
 
-    let resultSearch =  searchInput.length === 0 ? {propContact} : propContact.filter((contact)=>
-    (contact.name.toLowerCase().includes(searchInput.toLowerCase())
-    ));
-     console.log(resultSearch);
 
+    
 
     return (
         <div>
         <div className ="relative flex flex-row w-[150px] h-10 ml-4 mr-1 border-[#757474] rounded-xl bg-[#757474]">
         <img 
+        onClick={handellSearch}
         className="rounded-xl h-9 cursor-pointer"
         src="../img/icons8-search-30.png" 
         alt="search" />
         <input
-        value={searchInput}
         type="search"
         placeholder="جستجو ..."
         className ="fle
         x flex-row w-[120px] border-[#757474] rounded-xl bg-[#757474]"
+        value={searchInput}
         onChange={(s)=>{setSearchInput(s.target.value)}}
         />
+        </div>
+
+
+        <div>
+        {
+          <ul className="flex flex-col w-[200px] h-auto min-h-[30px] text-center m-3 bg-[#757474] cursor-pointer rounded-xl">
+          {searchResults.map((result) => (
+          <li key={result.id}>
+            {result.name}
+          </li>
+          ))}
+          </ul>
+        }
         </div>
 
 
