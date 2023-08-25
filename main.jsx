bf34107e819bbe4909c54a6ec24bd0f7687e1b76
nafiseh-charmchi <90713741+nafiseh-charmchi@users.noreply.github.com>
2023-08-26 00:34:44 +0330

    kameeelll

---
+++ main.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import FormAuthComponent from "./FormAuth";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <FormAuthComponent />
+  </React.StrictMode>
+);
+
