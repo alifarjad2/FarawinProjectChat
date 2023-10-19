5631abf37d30ec41e463acf8054312d07a5de15e
Reza Gholami <Reza.gholami.m.1998@gmail.com>
2023-10-19 11:53:08 +0330

    doc : for clean code export all imports files module  js and jsx to use another component

---
+++ Reza Gholami/src/components/index.js
@@ -0,0 +1,35 @@
+export { splitName } from "@/helpers";
+export { useStore } from "@/Store/Store";
+export { farawin } from "@/services/farawin";
+export { default as eye } from "@/assets/images/eye-fill.svg";
+export { default as spinner } from "@/assets/images/spinner.gif"
+export { default as InputChat } from "@/components/common/InputChat";
+export { default as InputForm } from "@/components/common/InputForm";
+export { default as messageBoxRadiusLeft } from "@/assets/images/pm.png"
+export { default as ButtonForm } from "@/components/common/ButtonForm";
+export { default as eyeSlash } from "@/assets/images/eye-slash-fill.svg";
+export { default as messageBoxRadiusRight } from "@/assets/images/pm-right.png"
+export { default as ChatBox } from "@/components/chat/components-chatPage/ChatBox";
+export { default as Contact } from "@/components/chat/components-chatPage/Contact";
+export { default as SenderChat } from "@/components/chat/components-chatPage/senderChat";
+export { default as ContactList } from "@/components/chat/components-chatPage/ContactList";
+export { default as ReceiverChat } from "@/components/chat/components-chatPage/ReceiverChat";
+export { default as SearchContact } from "@/components/chat/components-chatPage/SearchContact";
+export {
+  FaFacebookF,
+  FaTwitter,
+  FaGoogle,
+  FaLock,
+  FaUser,
+  FaUserMinus,
+  FaUserEdit,
+  FaSyncAlt,
+  FaLocationArrow,
+  FaEllipsisV,
+  FaSearch,
+  FaMobile,
+  FaUserPlus,
+  FaTimes,
+  FaTrash,
+  FaEdit,
+} from "react-icons/fa";
