235cdadfdaba90d31da18e9f5621bcca6cd0ffea
Hame:D <hamedsmart20@gmail.com>
2023-10-23 19:35:36 +0330

    update 1 aban

--- hamedAbdollahzade/src/components/ChatBoxReciver.jsx
+++ hamedAbdollahzade/src/components/ChatBoxReciver.jsx
@@ -10,13 +10,13 @@ export const ChatBoxReciver = (prop) => {
 
   return (
     <div className="flex flex-row-reverse items-end m-2 p-2">
-      <div className="shrink-0 rounded-lg ml-2 w-[40px] leading-[40px] text-center bg-slate-500">
+      <div className="shrink-0 rounded-full  w-[30px] leading-[30px] text-center bg-[#303952]">
         {prop.item.name[0] + prop.item.name[1]}
       </div>
-      <div className="flex flex-col bg-[#6B8AFE] mb-2 ml-1 rounded-xl p-2 max-w-[250px]">
-        <span className="text-xs">{prop.item.name} </span>
-        <p className="text-left pt-1 pb-1 break-words">{prop.reciver.text}</p>
-        <span className="text-start text-xs">
+      <div className="flex flex-col bg-[#303952] mb-2 ml-1 rounded-xl p-2 max-w-[60vh]">
+        <span className="text-[10px]">{prop.item.name} </span>
+        <p className="text-left p-2 pb-1 break-words">{prop.reciver.text}</p>
+        <span className="text-start mt-2 text-[10px]">
           {timeItem(prop.reciver.date)}
         </span>
       </div>
