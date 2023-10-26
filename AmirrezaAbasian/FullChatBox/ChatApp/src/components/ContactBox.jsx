4865341035c6837bd4d6c5af8d9597729fdecfea
Amirreza <a.abasian1382@gmail.com>
2023-10-26 21:27:12 +0330

    Login works properly

--- AmirrezaAbasian/FullChatBox/ChatApp/src/components/ContactBox.jsx
+++ AmirrezaAbasian/FullChatBox/ChatApp/src/components/ContactBox.jsx
@@ -1,39 +1,71 @@
 // import farawin from "farawin"
-import { useState, useEffect } from "react"
+import { useState, useEffect } from "react";
 
-const ContactBox = ({ lastMessage, setLastMessage, chatName, setChatName, className, messageReceiver, setMessageReceiver, name, phoneNumber, isContactList, setIsContactList }) => {
+const ContactBox = ({
+  lastMessage,
+  setLastMessage,
+  chatName,
+  setChatName,
+  className,
+  messageReceiver,
+  setMessageReceiver,
+  name,
+  phoneNumber,
+  isContactList,
+  setIsContactList,
+}) => {
+  const handleClick = () => {
+    // console.log(phoneNumber);
+    setMessageReceiver(phoneNumber);
+    setIsContactList(false);
+    setChatName(name); // here to fix the date and last sent message
+  };
 
+  console.log(lastMessage);
+  const personChats = lastMessage?.chatList?.filter(
+    (row) => row.receiver === messageReceiver
+  );
+  const _lastMessage = personChats?.reverse()[0];
+  console.log(personChats);
 
-    const handleClick = () => {
-        // console.log(phoneNumber);
-        setMessageReceiver(phoneNumber)
-        setIsContactList(false)
-        setChatName(name) // here to fix the date and last sent message
-    }
-    const _lastMessage = lastMessage?.text
-
-    return (
-        <div onClick={handleClick} className={className}>
-            <div className="bg-[#202329] mb-3 rounded-xl mx-3 h-20 flex flex-row justify-between transition duration-300 ease-in cursor-pointer group hover:bg-[#2E333D]">
-                <div className="basis-3/12 bg-transparent rounded-lg text-center flex">
-                    <div className="bg-green-200 group-hover:opacity-50 flex m-2 items-center justify-center rounded-[19px] w-7/12 h-7/12 min-w-[64px] min-h-[40px]">
-                        <span className="text-stone-900 text-2xl font-extrabold font-serif text-center">{name.charAt(0)}</span>
-                    </div>
-                </div>
-                <div className="basis-8/12 overflow-x-hidden bg-transparent rounded-lg pl-1 flex flex-col justify-around">
-                    <div className="pt-2 truncate">
-                        {name}
-                    </div>
-                    <div className="pb-2 truncate">
-                        {lastMessage?.receiver === messageReceiver ? _lastMessage : 'هنوز پیامی ندادید'}
-                    </div>
-                </div>
-                <div className="mr-5 group-hover:text-[#509ef1] group-hover:transition basis-1/12 bg-transparent rounded-lg text-center text-clip font-serif text-sm">
-                    <span>{new Date(lastMessage?.date).getHours() + ':' + new Date(lastMessage?.date).getMinutes().toString().padStart(2, '0') + '     ' + new Date(lastMessage?.date).getDate() + '/' + new Date(lastMessage?.date).getMonth().toString().padStart(2, '0')}</span>
-                </div>
-            </div>
+  return (
+    <div onClick={handleClick} className={className}>
+      <div className="bg-[#202329] mb-3 rounded-xl mx-3 h-20 flex flex-row justify-between transition duration-300 ease-in cursor-pointer group hover:bg-[#2E333D]">
+        <div className="basis-3/12 bg-transparent rounded-lg text-center flex">
+          <div className="bg-green-200 group-hover:opacity-50 flex m-2 items-center justify-center rounded-[19px] w-7/12 h-7/12 min-w-[64px] min-h-[40px]">
+            <span className="text-stone-900 text-2xl font-extrabold font-serif text-center">
+              {name.charAt(0)}
+            </span>
+          </div>
         </div>
-    )
-}
+        <div className="basis-8/12 overflow-x-hidden bg-transparent rounded-lg pl-1 flex flex-col justify-around">
+          <div className="pt-2 truncate">{name}</div>
+          <div className="pb-2 truncate">
+            {lastMessage?.receiver === messageReceiver
+              ? _lastMessage
+              : "هنوز پیامی ندادید"}
+          </div>
+        </div>
+        <div className="mr-5 group-hover:text-[#509ef1] group-hover:transition basis-1/12 bg-transparent rounded-lg text-center text-clip font-serif text-sm">
+          <span>
+            {new Date(lastMessage?.date).getHours() +
+              ":" +
+              new Date(lastMessage?.date)
+                .getMinutes()
+                .toString()
+                .padStart(2, "0") +
+              "     " +
+              new Date(lastMessage?.date).getDate() +
+              "/" +
+              new Date(lastMessage?.date)
+                .getMonth()
+                .toString()
+                .padStart(2, "0")}
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default ContactBox
+export default ContactBox;
--- AmirrezaAbasian/FullChatBox/ChatApp/src/components/ContactsList.jsx
+++ AmirrezaAbasian/FullChatBox/ChatApp/src/components/ContactsList.jsx
@@ -1,107 +1,194 @@
-import farawin from "farawin"
-import SearchIcon from "../assets/SearchIcon"
-import ContactBox from "./ContactBox"
-import AddContactIcon from "../assets/AddContactIcon"
-import { useState, useEffect } from "react"
+import farawin from "farawin";
+import SearchIcon from "../assets/SearchIcon";
+import ContactBox from "./ContactBox";
+import AddContactIcon from "../assets/AddContactIcon";
+import { useState, useEffect } from "react";
 
+const ContactsList = ({
+  chatName,
+  setChatName,
+  isContactList,
+  setIsContactList,
+  messageReceiver,
+  setMessageReceiver,
+}) => {
+  // #region Stated
+  const [isAddingContact, setIsAddingContact] = useState(false);
+  const [newUserName, setNewUserName] = useState("");
+  const [newUserPhoneNumber, setNewUserPhoneNumber] = useState("");
+  const [isValidPhoneNumber, setIsValidPhoneNumber] = useState(false);
+  const [personalContacts, setPersonalContacts] = useState([]);
+  const [lastMessage, setLastMessage] = useState([]);
+  const [searchInput, setSearchInput] = useState("");
+  const [isSearching, setIsSearching] = useState(false);
+  // #endregion
 
-const ContactsList = ({ chatName, setChatName, isContactList, setIsContactList, messageReceiver, setMessageReceiver }) => {
-    // #region Stated
-    const [isAddingContact, setIsAddingContact] = useState(false)
-    const [newUserName, setNewUserName] = useState('')
-    const [newUserPhoneNumber, setNewUserPhoneNumber] = useState('')
-    const [isValidPhoneNumber, setIsValidPhoneNumber] = useState(false)
-    const [personalContacts, setPersonalContacts] = useState([])
-    const [lastMessage, setLastMessage] = useState([])
-    const [searchInput, setSearchInput] = useState('')
-    const [isSearching, setIsSearching] = useState(false)
-    // #endregion
+  // For phoneNumber validation ↓↓
+  useEffect(() => {
+    const phoneNumberPattern = farawin.mobileRegex;
+    const isValid = phoneNumberPattern.test(newUserPhoneNumber);
+    setIsValidPhoneNumber(isValid);
+  }, [newUserPhoneNumber]);
+  // -------------------------------------------------------------
 
-    // For phoneNumber validation ↓↓
-    useEffect(() => {
-        const phoneNumberPattern = farawin.mobileRegex;
-        const isValid = phoneNumberPattern.test(newUserPhoneNumber);
-        setIsValidPhoneNumber(isValid);
-    }, [newUserPhoneNumber]);
-    // -------------------------------------------------------------
+  // For getting user's contacts ↓↓
+  useEffect(() => {
+    const gettingContacts = async () => {
+      const rawData = await farawin.getContacts((res) => res);
+      const personalContacts = rawData.contactList.filter(
+        (row) => row.ref === localStorage.userPhoneNumber
+      );
+      setPersonalContacts(personalContacts);
+      // console.log(personalContacts);
+    };
 
-    // For getting user's contacts ↓↓
-    useEffect(() => {
-        const gettingContacts = async () => {
-            const rawData = await farawin.getContacts(res => res)
-            const personalContacts = rawData.contactList.filter((row) => row.ref === localStorage.userPhoneNumber)
-            setPersonalContacts(personalContacts)
-            // console.log(personalContacts);
-        }
+    const gettingLastMessage = async () => {
+      const rawData = await farawin.getChats();
+      const personChats = rawData.chatList.filter(
+        (row) => row.receiver === messageReceiver
+      );
+      //   console.log(rawData);
+      //   setLastMessage(personChats.reverse()[0]);
+      setLastMessage(rawData);
+    };
+    gettingLastMessage();
+    gettingContacts();
+  }, []);
+  // -------------------------------------------------------------
 
-        const gettingLastMessage = async () => {
-            const rawData = await farawin.getChats()
-            const personChats = rawData.chatList.filter((row) => row.receiver === messageReceiver)
-            // console.log(personChats);
-            setLastMessage(personChats.reverse()[0])
-        }
-        gettingLastMessage()
-        gettingContacts();
-    }, [])
-    // -------------------------------------------------------------
+  useEffect(() => {
+    searchInput.length === 0 && setIsSearching(false);
+    // console.log(searchInput);
+  }, [searchInput]);
 
-    useEffect(() => {
-        searchInput.length === 0 && setIsSearching(false)
-        // console.log(searchInput);
-    }, [searchInput])
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    farawin.testAddContact(newUserPhoneNumber, newUserName, (res) => {
+      console.log(res.message);
+      setNewUserPhoneNumber("");
+      setNewUserName(res.message);
+    });
+    setTimeout(() => {
+      setNewUserName("");
+      setNewUserPhoneNumber("");
+      setIsAddingContact(false);
+    }, 3000);
+  };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        farawin.testAddContact(newUserPhoneNumber, newUserName, (res) => { console.log(res.message); setNewUserPhoneNumber(''); setNewUserName(res.message) })
-        setTimeout(() => {
-            setNewUserName('')
-            setNewUserPhoneNumber('')
-            setIsAddingContact(false)
-        }, 3000);
-    }
-
-
-
-    return (
-        <div className="flex flex-col h-full">
-            <div className='h-[10%] mb-2 w-full'>
-                <div id="search-box" className="pb-1">
-                    <div className="bg-[#2E333D] transition duration-500 focus-within:bg-[#353b46] mx-5 my-4 rounded-xl h-14 flex items-center focus-within:ring-2 shadow-inner focus-within:ring-sky-900">
-                        <button onClick={() => setIsSearching((e) => e = true)} className='mr-6'>
-                            <SearchIcon className={'text-2xl hover:opacity-60'} />
-                        </button>
-                        <input value={searchInput} onChange={(e) => setSearchInput(e.target.value)} type="text" placeholder="Search" className="text-white font-medium text-lg font-mono w-full h-[30px] placeholder:italic outline-none placeholder:opacity-70 rounded-md bg-transparent mx-2" />
-                    </div>
-                </div>
-            </div>
-            <div className='h-[80%] w-full overflow-y-auto'>
-                {/* <ContactBox isContactList={isContactList} setIsContactList={setIsContactList} className={'mt-6 text-white'} /> */}
-                {
-                    (personalContacts && !isSearching) && personalContacts.map((row) => {
-                        return (
-                            <ContactBox lastMessage={lastMessage} setLastMessage={setLastMessage} chatName={chatName} setChatName={setChatName} messageReceiver={messageReceiver} setMessageReceiver={setMessageReceiver} phoneNumber={row.username} key={row.date.toString()} name={row.name} isContactList={isContactList} setIsContactList={setIsContactList} className={'mt-6 text-white'} />
-                        )
-                    })
-                }
-                {
-                    (personalContacts && isSearching) && (personalContacts.filter((row) => row.name.toLowerCase() === searchInput.toLowerCase()))?.map((row) => <ContactBox lastMessage={lastMessage} setLastMessage={setLastMessage} chatName={chatName} setChatName={setChatName} messageReceiver={messageReceiver} setMessageReceiver={setMessageReceiver} phoneNumber={row.username} key={row.date.toString()} name={row.name} isContactList={isContactList} setIsContactList={setIsContactList} className={'mt-6 text-white'} />)
-                }
-            </div>
-            {
-                !isAddingContact ? (
-                    <div onClick={() => setIsAddingContact(true)} title='افزودن مخاطب' className='h-[10%] w-full flex justify-center pt-6'>
-                        <AddContactIcon className={'text-4xl hover:opacity-60 transition cursor-pointer'} />
-                    </div>
-                ) : (
-                    <form onSubmit={handleSubmit} className="flex justify-around items-center md:mt-4">
-                        <div><button disabled={(isValidPhoneNumber && newUserName.length !== 0) ? false : true} type="submit" className="bg-[#2E333D] rounded-3xl p-2 text-white hover:opacity-70 transition-opacity disabled:opacity-30 disabled:cursor-not-allowed ml-4">افزودن</button></div>
-                        <div><input onChange={(e) => setNewUserName(e.target.value)} value={newUserName} className="bg-[#2E333D] rounded-2xl h-10 outline-none text-white transition focus:outline-2 focus:outline w-10/12 md:w-11/12 -outline-offset-1 focus:outline-slate-800 md:px-2" type="text" placeholder="     نام مخاطب" /></div>
-                        <div><input onChange={(e) => setNewUserPhoneNumber(e.target.value)} value={newUserPhoneNumber} className="bg-[#2E333D] rounded-2xl h-10 outline-none text-white transition focus:outline-2 focus:outline w-10/12 md:w-11/12 -outline-offset-1 focus:outline-slate-800 md:px-2" type="tel" placeholder="     تلفن مخاطب" /></div>
-                    </form>
-                )
-            }
+  return (
+    <div className="flex flex-col h-full">
+      <div className="h-[10%] mb-2 w-full">
+        <div id="search-box" className="pb-1">
+          <div className="bg-[#2E333D] transition duration-500 focus-within:bg-[#353b46] mx-5 my-4 rounded-xl h-14 flex items-center focus-within:ring-2 shadow-inner focus-within:ring-sky-900">
+            <button
+              onClick={() => setIsSearching((e) => (e = true))}
+              className="mr-6"
+            >
+              <SearchIcon className={"text-2xl hover:opacity-60"} />
+            </button>
+            <input
+              value={searchInput}
+              onChange={(e) => setSearchInput(e.target.value)}
+              type="text"
+              placeholder="Search"
+              className="text-white font-medium text-lg font-mono w-full h-[30px] placeholder:italic outline-none placeholder:opacity-70 rounded-md bg-transparent mx-2"
+            />
+          </div>
         </div>
-    )
-}
+      </div>
+      <div className="h-[80%] w-full overflow-y-auto">
+        {/* <ContactBox isContactList={isContactList} setIsContactList={setIsContactList} className={'mt-6 text-white'} /> */}
+        {personalContacts &&
+          !isSearching &&
+          personalContacts.map((row) => {
+            return (
+              <ContactBox
+                lastMessage={lastMessage}
+                setLastMessage={setLastMessage}
+                chatName={chatName}
+                setChatName={setChatName}
+                messageReceiver={messageReceiver}
+                setMessageReceiver={setMessageReceiver}
+                phoneNumber={row.username}
+                key={row.date.toString()}
+                name={row.name}
+                isContactList={isContactList}
+                setIsContactList={setIsContactList}
+                className={"mt-6 text-white"}
+              />
+            );
+          })}
+        {personalContacts &&
+          isSearching &&
+          personalContacts
+            .filter(
+              (row) => row.name.toLowerCase() === searchInput.toLowerCase()
+            )
+            ?.map((row) => (
+              <ContactBox
+                lastMessage={lastMessage}
+                setLastMessage={setLastMessage}
+                chatName={chatName}
+                setChatName={setChatName}
+                messageReceiver={messageReceiver}
+                setMessageReceiver={setMessageReceiver}
+                phoneNumber={row.username}
+                key={row.date.toString()}
+                name={row.name}
+                isContactList={isContactList}
+                setIsContactList={setIsContactList}
+                className={"mt-6 text-white"}
+              />
+            ))}
+      </div>
+      {!isAddingContact ? (
+        <div
+          onClick={() => setIsAddingContact(true)}
+          title="افزودن مخاطب"
+          className="h-[10%] w-full flex justify-center pt-6"
+        >
+          <AddContactIcon
+            className={"text-4xl hover:opacity-60 transition cursor-pointer"}
+          />
+        </div>
+      ) : (
+        <form
+          onSubmit={handleSubmit}
+          className="flex justify-around items-center md:mt-4"
+        >
+          <div>
+            <button
+              disabled={
+                isValidPhoneNumber && newUserName.length !== 0 ? false : true
+              }
+              type="submit"
+              className="bg-[#2E333D] rounded-3xl p-2 text-white hover:opacity-70 transition-opacity disabled:opacity-30 disabled:cursor-not-allowed ml-4"
+            >
+              افزودن
+            </button>
+          </div>
+          <div>
+            <input
+              onChange={(e) => setNewUserName(e.target.value)}
+              value={newUserName}
+              className="bg-[#2E333D] rounded-2xl h-10 outline-none text-white transition focus:outline-2 focus:outline w-10/12 md:w-11/12 -outline-offset-1 focus:outline-slate-800 md:px-2"
+              type="text"
+              placeholder="     نام مخاطب"
+            />
+          </div>
+          <div>
+            <input
+              onChange={(e) => setNewUserPhoneNumber(e.target.value)}
+              value={newUserPhoneNumber}
+              className="bg-[#2E333D] rounded-2xl h-10 outline-none text-white transition focus:outline-2 focus:outline w-10/12 md:w-11/12 -outline-offset-1 focus:outline-slate-800 md:px-2"
+              type="tel"
+              placeholder="     تلفن مخاطب"
+            />
+          </div>
+        </form>
+      )}
+    </div>
+  );
+};
 
-export default ContactsList
+export default ContactsList;
